34ebfe466982d63941c28f00924431b3dc8edbaf
robertuptc <robertuptc@gmail.com>
2025-03-31 15:49:53 -0400

    js problem finished

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-console.log("HELLO PAPA PLATOON!")
-// Your function(s) should go here that will interact with the webpage or DOM
+function randomNum() {
+    return Math.floor(Math.random() * (3 - 1) + 1)
+};
+
+const form = document.querySelector('form');
+let div = document.getElementById('user-feedback')
+let p = document.createElement('p')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let userNumber = e.target.elements[0].value
+    if (Number(userNumber) == test) {
+        p.innerHTML = 'You have WON!!!'
+        div.appendChild(p)
+    } else if (Number(userNumber) < test) {
+        p.innerHTML = 'Guess Higher'
+        div.appendChild(p)
+    } else {
+        p.innerHTML = 'Guess Lower'
+        div.appendChild(p)
+
+    }
+});
+let test= randomNum()
+console.log('randomNum',test)
